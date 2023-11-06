d69aa1803fac247a1732b901fb32da95ac925250
Mashanz <mangatkk@gmail.com>
2023-11-06 14:41:58 +0700

    fix: move templates and static

--- app/app/settings.py
+++ app/app/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "base.apps.BaseConfig",
     "api.apps.ApiConfig",
     "home.apps.HomeConfig",
 ]
