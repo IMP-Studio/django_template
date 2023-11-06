dfd453f705a56a6fc73aa3551a64c76682539da5
Mashanz <mangatkk@gmail.com>
2023-11-06 10:27:11 +0700

    fix: .env positions

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ env = environ.Env(
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+environ.Env.read_env(os.path.join(BASE_DIR.parent, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
